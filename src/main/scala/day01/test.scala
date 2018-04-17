0f59b892059c84310dd35d42bde7a40d9c626b28
songzhifei <zhifeisong@163.com>
2018-04-17 14:22:39 +0800

    test

---
+++ src/main/scala/day01/test.scala
@@ -0,0 +1,7 @@
+package day01
+
+object test {
+  def main(args: Array[String]): Unit = {
+    println("hello world!")
+  }
+}
---
+++ src/main/scala/day06/SparkWC.scala
@@ -0,0 +1,25 @@
+package day06
+import org.apache.spark.rdd.RDD
+import org.apache.spark.{SparkConf, SparkContext}
+
+object SparkWC {
+  def main(args: Array[String]): Unit = {
+    val conf: SparkConf = new SparkConf().setAppName("SparkWC").setMaster("local[*]")
+
+    val sc: SparkContext = new SparkContext(conf)
+
+    var lines = sc.textFile(args(0))
+
+    var words: RDD[String] = lines.flatMap(_.split(" "))
+
+    var paird: RDD[(String, Int)] = words.map((_, 1))
+
+    var reduced: RDD[(String, Int)] = paird.reduceByKey(_ + _)
+
+    var res: RDD[(String, Int)] = reduced.sortBy(_._2, false)
+
+    println(res.collect().toBuffer)
+
+    sc.stop()
+  }
+}
