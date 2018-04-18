8b2b5bf2044b7ad26213e9460f9a3a7419ab4111
songzhifei <zhifeisong@163.com>
2018-04-18 17:28:38 +0800

    test

--- src/main/scala/day01/test.scala
+++ src/main/scala/day01/test.scala
@@ -1,7 +1,21 @@
 package day01
 
+import org.apache.spark.{SparkConf, SparkContext}
+
 object test {
   def main(args: Array[String]): Unit = {
-    println("hello world!")
+    val conf: SparkConf = new SparkConf().setAppName("Sparktest").setMaster("local")
+
+    val sc: SparkContext = new SparkContext(conf)
+
+    val words = sc.parallelize(List("a b c","d e f","m n j"))
+
+    val length = words.map(s=>s.length)
+    //    println(length.collect().toBuffer)
+    val i: Int = length.reduce((a, b)=>a+b)
+    println(i)
+    //val res1 = words.flatMap(_.split(" "))
+
+    //println(res1.collect().toBuffer)
   }
 }
